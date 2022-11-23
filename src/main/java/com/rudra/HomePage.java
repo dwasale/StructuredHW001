6ab4a2ba56195ca03f8652dbd67fcc19f2913702
Rudra <dwasale@gmail.com>
2022-11-23 11:20:09 +0000

    alert pop up window commit

--- src/main/java/com/rudra/HomePage.java
+++ src/main/java/com/rudra/HomePage.java
@@ -1,8 +1,24 @@
 package com.rudra;
 
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.testng.Assert;
 
-public class HomePage {
+public class HomePage extends Utils{
 
-    private static WebDriver driver;
+    private By _voteButton = By.id("vote-poll-1");
+
+    //To check the correct url
+    public void toVerifyBrowserHasCorrectURL(){
+        String URL = driver.getCurrentUrl();
+        Assert.assertEquals(URL,"https://demo.nopcommerce.com/","Url didn't match");
+        System.out.println(URL);
+    }
+
+    public void toVerifyAlertPopUpWindowAfterClickingVoteButtonWithoutSelectingAny(){
+        clickOnElement(_voteButton);
+    }
+
+    public void toCheckAlertIsPresent(){
+        Assert.assertTrue(isAlertPresent(),"Alert popup window is present");
+    }
 }
